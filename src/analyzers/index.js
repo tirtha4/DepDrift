01a42d8b1524615b074bd22ba24aad889dc313f8
Tirtha Sarker <tsark99@gmail.com>
2025-04-30 21:14:37 +0530

    refactor: update analyzer index with improved exports and error handling

--- src/analyzers/index.js
+++ src/analyzers/index.js
@@ -1,62 +1,62 @@
 /**
  * DepDrift Analyzers Module
- * 
+ *
  * This module exports the dependency analyzers that form the core functionality
  * of DepDrift.
- * 
+ *
  * @module analyzers
  */
 
 'use strict';
 
 // Import from local files in the analyzers directory
-const { 
-  analyzePackage, 
-  calculateDriftLevel, 
-  analyzeDependency, 
+const {
+  analyzePackage,
+  calculateDriftLevel,
+  analyzeDependency,
   fetchNpmPackageInfo,
   parseVersionRange,
   isPreRelease,
   satisfiesRange
 } = require('./packageAnalyzer');
 
-const { 
-  checkVulnerabilities, 
-  analyzeSecurity, 
-  VULNERABILITY_SOURCES 
+const {
+  checkVulnerabilities,
+  analyzeSecurity,
+  VULNERABILITY_SOURCES
 } = require('./securityAnalyzer');
 
-const { 
-  analyzeDrift, 
-  flattenTree, 
-  classifyVersionDifference 
+const {
+  analyzeDrift,
+  flattenTree,
+  classifyVersionDifference
 } = require('./driftAnalyzer');
 
 /**
  * Analyze a package for dependency drift and security issues
- * 
+ *
  * @param {Object} packageInfo - The parsed package.json object
  * @param {Object} options - Analysis options
  * @returns {Promise<Object>} Analysis results
  */
-async function analyzeDependencies(packageInfo, options = {}) {
+async function analyzeDependencies (packageInfo, options = {}) {
   // This wrapper function combines drift and security analysis
   const { includeSecurity = true, ...restOptions } = options;
-  
+
   // Get drift analysis
   const driftResults = await analyzePackage(packageInfo, restOptions);
-  
+
   if (!includeSecurity) {
     return driftResults;
   }
-  
+
   // Add security analysis
   const securityResults = await analyzeSecurity(driftResults, {
     sources: options.securitySources || ['NPM_AUDIT'],
     useCache: options.useCache !== false,
     maxConcurrent: options.maxConcurrent || 5
   });
-  
+
   // Merge the results
   return {
     ...driftResults,
@@ -70,7 +70,7 @@ module.exports = {
   analyzeDrift,
   analyzeSecurity,
   analyzeDependencies,
-  
+
   // Utility functions
   checkVulnerabilities,
   calculateDriftLevel,
@@ -81,7 +81,7 @@ module.exports = {
   parseVersionRange,
   isPreRelease,
   satisfiesRange,
-  
+
   // Constants
   VULNERABILITY_SOURCES
-}; 
+};
