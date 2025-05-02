7bed6ee43df2d0e605a09ddab2952a8b0094a315
Tirtha Sarker <tsark99@gmail.com>
2025-05-02 14:01:10 +0530

    Update project configuration files for test compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,60 +1,30 @@
 /**
- * Jest configuration file for DepDrift
+ * Jest configuration
+ * 
+ * This configuration is designed to work with ES Modules.
  */
+
 export default {
-  // The root directory containing tests
-  roots: ['<rootDir>/test/'],
-  
-  // The test environment
+  // Test environment
   testEnvironment: 'node',
   
-  // The file extensions to look for
-  moduleFileExtensions: ['js', 'json'],
-  
-  // The pattern to find test files
-  testMatch: ["**/test/**/*.test.js"],
-  
-  // Files to ignore
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage/',
-    '/dist/',
-    '/test/fixtures/'
-  ],
-  
-  // Collect coverage information
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!**/node_modules/**'
-  ],
-  
-  // Coverage output format
-  coverageReporters: ['text', 'lcov', 'html'],
-  
-  // Set coverage thresholds to start with reasonable goals
-  coverageThreshold: {
-    global: {
-      branches: 2,
-      functions: 2,
-      lines: 5,
-      statements: 5
-    }
-  },
-  
-  // Add test environment setup file
-  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
-  
-  // Verbose test output
+  // Display individual test results
   verbose: true,
   
-  // Clear the console before each test run
-  clearMocks: true,
-  
-  // The directory to store coverage reports
-  coverageDirectory: 'coverage',
-  
-  // Configure to work with ES Modules
+  // Support ES Modules - .js is inferred from package.json type
   transform: {},
-  extensionsToTreatAsEsm: ['.js']
+  
+  // Test matching pattern
+  testMatch: ['**/test/**/*.test.js'],
+  
+  // Collect coverage information
+  collectCoverage: false,
+  
+  // Setup file to run before tests
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  
+  // Global variables available in tests
+  globals: {
+    '__DEV__': true
+  }
 }; 
